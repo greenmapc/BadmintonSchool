86dd92d0470f4dc641f4b3e7dc5beb11a5cc2e4c
greenmapc <greenmapc@gmail.com>
2019-04-29 01:34:23 +0300

    added properties to .gitigone, created dist files

--- src/main/java/ru/kpfu/itis/greenmapc/service/UserService.java
+++ src/main/java/ru/kpfu/itis/greenmapc/service/UserService.java
@@ -1,18 +1,10 @@
 package ru.kpfu.itis.greenmapc.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 import ru.kpfu.itis.greenmapc.form.SettingsForm;
 import ru.kpfu.itis.greenmapc.form.SignUpForm;
 import ru.kpfu.itis.greenmapc.model.User;
-import ru.kpfu.itis.greenmapc.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
+
 import java.util.Optional;
 
 public interface UserService {
